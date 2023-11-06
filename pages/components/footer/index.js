ef4ff0775e660fa7ea507d52c09aa62ff66d3cea
JOANDERSON ALVES MARTINS <joandersonmartins2013@gmail.com>
2023-11-06 18:40:35 -0300

    feat: created footer/index.js
    Created footer/index.js to page

---
+++ pages/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-600 p-8 text-center shadow-outline">
+        <div className="container mx-auto text-white font-semibold">
+            Todos os direitos reservados por: Dev FullStack <a className=' hover:underline font-bold' href='#'>Joanderson Alves </a> | <a className=' hover:underline font-bold' href="https://www.linkedin.com/in/joandersonalvesmartins/">Linkedin</a> | <a className=' hover:underline font-bold' href="https://github.com/JoandersonAlvesMartins">GitHub</a>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
